e5a79650fdbb897f07bc3589d236213001cfe4f7
Manuel <musoxvi@gmail.com>
2018-10-31 01:13:07 -0300

    Repo React

---
+++ src/components/Title.js
@@ -0,0 +1,16 @@
+import React, { Fragment } from 'react';
+
+
+export default class Title extends React.Component {
+  render() {
+    return (
+      <Fragment>
+        <h1>Places</h1>
+        <p>Descubre lugares de manera simple</p>
+      </Fragment>
+
+    )
+  }
+}
+
+
