2485d7a1c05423a95870bf0000dc0c0476be9614
ManuelUso <musoxvi@gmail.com>
2018-11-11 21:39:26 -0300

    flexboxgrid

--- src/App.js
+++ src/App.js
@@ -1,55 +1,84 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import RaisedButton from 'material-ui/RaisedButton';
+import { Card, CardText } from 'material-ui/Card'
+
+import {indigo400, redA400, lightBlueA400, amberA400} from 'material-ui/styles/colors'
 
 import Title from './components/Title';
 
-import { hola } from './components/Title';
+import './App.css';
+//import { hola } from './components/Title';
 
 class App extends Component {
 
   constructor(props) {
     super(props);
 
-    this.state = {
-      numero: 0
-    };
-
-    this.updateNumber = this.updateNumber.bind(this);
   }
 
-  updateNumber() {
-    this.setState({
-      numero: this.state.numero + 1
-    })
-  }
 
   render() {
     return (
-      <section>
-        <div>
-          <Title></Title>
-          <h2>{this.state.numero}</h2>
-          <button onClick={ this.updateNumber }>Crear cuenta gratuita</button>
-          <img src={ process.env.PUBLIC_URL + '/images/top-background.jpeg'} alt=""/>
-          <div>
-            <ul>
-              <li>
-                <h3>Califica con emociones</h3>
-                <p>Califica tus lugares con experiencias, no con numeros</p>
-              </li>
-              <li>
-                <h3>Sin internet? Sin problemas</h3>
-                <p>Places funciona sin internet o con conexiones lentas</p>
-              </li>
-              <li>
-                <h3>Tus lugares favoritos</h3>
-                <p>Define tu lista de sitios favoritos</p>
-              </li>
-            </ul>
+      <MuiThemeProvider>
+        <div className='Header-background'>
+          <div style={{'width':'80%', 'margin':'0 auto'}}>
+            <div className='header-name'>
+              <Title></Title>
+              <RaisedButton label='Crear cuenta gratuita' secondary={ true }/>
+              <img className='Header-illustration' src={ process.env.PUBLIC_URL + '/images/top-background.jpg'} alt=""/>
+            </div>
+            <div>
+              <ul>
+                <Card className='Header-Benefit'>
+                  <CardText>
+                    <div className='row'>
+                      <div className='Header-Benefit-image'  style={{'backgroundColor': redA400}}>
+                        <img src={ process.env.PUBLIC_URL + '/images/corazon.png'} alt=""/>
+                      </div>
+                      <div className='Header-Benefit-content'>
+                        <h3>Califica con emociones</h3>
+                        <p>Califica tus lugares con experiencias, no con numeros</p>
+                      </div>
+                    </div>
+                  </CardText>
+                </Card>
+
+                <Card className='Header-Benefit'>
+                  <CardText>
+                    <div className='row'>
+                      <div className='Header-Benefit-image'  style={{'backgroundColor': lightBlueA400}}>
+                        <img src={ process.env.PUBLIC_URL + '/images/wifi.png'} alt=""/>
+                      </div>
+                      <div className='Header-Benefit-content'>
+                      <h3>Sin internet? Sin problemas</h3>
+                      <p>Places funciona sin internet o con conexiones lentas</p>
+                      </div>
+                    </div>
+                  </CardText>
+                </Card>
+
+                <Card className='Header-Benefit'>
+                  <CardText>
+                    <div className='row'>
+                      <div className='Header-Benefit-image'  style={{'backgroundColor': amberA400}}>
+                        <img src={ process.env.PUBLIC_URL + '/images/estrella.png'} alt=""/>
+                      </div>
+                      <div className='Header-Benefit-content'>
+                      <h3>Tus lugares favoritos</h3>
+                    <p>Define tu lista de sitios favoritos</p>
+                      </div>
+                    </div>
+                  </CardText>
+                </Card>
+              </ul>
+            </div>
           </div>
         </div>
-      </section>
+        <div style={{'background-color':indigo400, 'padding':'50px'}}>
+          
+        </div>
+      </MuiThemeProvider>
     );
   }
 }
